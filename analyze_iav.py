b975892d6476f193119c239c2017b1bbbb062bab
fervenegas1125 <fer.venegas1125@gmail.com>
2026-06-04 20:23:42 -0600

    feat: funcion classify_gene()

--- analyze_iav.py
+++ analyze_iav.py
@@ -12,8 +12,24 @@ def is_significant(log2_fold_change, padj, lfc_threshold, padj_threshold):
     return False
 
 
-# ejemplo para probar la funcion:
+# ejemplo para probar is_significant():
 
 print(is_significant(4.2, 0.0001, 1, 0.05))
 print(is_significant(0.3, 0.0001, 1, 0.05))
 print(is_significant(3.0, 0.8, 1, 0.05))
+
+
+# 2. classify_gene()
+
+
+def classify_gene(log2_fold_change):
+    """Clasifica un gen significativo."""
+
+    if log2_fold_change > 0:
+        return "upregulated"
+
+    return "downregulated"
+
+# ejemplo para probar classify_gene():
+print(classify_gene(4.2))
+print(classify_gene(-3.0))
